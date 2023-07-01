d8e226ef69f67bc4b9c1fb80f833b98a73581b51
amnaalykhan <85075810+amnaalykhan@users.noreply.github.com>
2023-07-01 14:40:58 +0530

    Update t20_wc_match_results.js

--- web_scrapping_codes/t20_wc_match_results.js
+++ web_scrapping_codes/t20_wc_match_results.js
@@ -1,36 +1,26 @@
-/* -------------- STAGE 1 ------------ */
-
-//------- 1.a Interaction Code ------ //
-
-navigate('https://stats.espncricinfo.com/ci/engine/records/team/match_results.html?id=14450;type=tournament');
-collect(parse());
+import scrapy
 
 
+class CricketSpider(scrapy.Spider):
+    name = 'cricket_spider'
+    start_urls = [
+        'https://stats.espncricinfo.com/ci/engine/records/team/match_results.html?id=14450;type=tournament'
+    ]
 
+    def parse(self, response):
+        # Parsing the page to collect match summary
+        match_summary = []
+        all_rows = response.css('table.engineTable > tbody > tr.data1')
+        for row in all_rows:
+            tds = row.css('td')
+            match_summary.append({
+                "team1": tds[0].css('::text').get(),
+                "team2": tds[1].css('::text').get(),
+                "winner": tds[2].css('::text').get(),
+                "margin": tds[3].css('::text').get(),
+                "ground": tds[4].css('::text').get(),
+                "matchDate": tds[5].css('::text').get(),
+                "scorecard": tds[6].css('::text').get()
+            })
 
-//------- 1.b Parser Code ------------//
-//Step1: create an array to store all the records
-let matchSummary = []
-
-//Step2: Selecting all rows we need from target table
-const allRows = $('table.engineTable > tbody > tr.data1');
-
-//Step3: Looping through each rows and get the data from the cells(td)
- allRows.each((index, element) => {
- 		const tds = $(element).find('td');   //find the td
-		matchSummary.push({
-              'team1':  $(tds[0]).text(),
-              'team2':  $(tds[1]).text(),
-              'winner':  $(tds[2]).text(),
-              'margin':  $(tds[3]).text(),
-              'ground': $(tds[4]).text(),
-              'matchDate': $(tds[5]).text(),
-              'scorecard':   $(tds[6]).text() 
-		})   
- })
-
-
-// step4: Finally returning the data
-return {
-  "matchSummary": matchSummary
-};
+        yield {"matchSummary": match_summary}
