c7bad721c90458e1216201e77bb43dadc861fd0f
amnaalykhan <85075810+amnaalykhan@users.noreply.github.com>
2023-07-01 14:39:35 +0530

    Update t20_wc_bowling_summary.js

--- web_scrapping_codes/t20_wc_bowling_summary.js
+++ web_scrapping_codes/t20_wc_bowling_summary.js
@@ -1,89 +1,79 @@
-/* -------------- STAGE 1 ------------ */
-
-//------- 1.a Interaction Code ------ //
-navigate('https://stats.espncricinfo.com/ci/engine/records/team/match_results.html?id=14450;type=tournament');
+import scrapy
 
 
-let links = parse().playersLinks;
-for(let i of links) { 
-  next_stage({url: i}) 
-}
+class CricketSpider(scrapy.Spider):
+    name = 'cricket_spider'
+    start_urls = [
+        'https://stats.espncricinfo.com/ci/engine/records/team/match_results.html?id=14450;type=tournament'
+    ]
 
-//------- 1.b Parser Code ------------//
-let links = []
-const allRows = $('table.engineTable > tbody > tr.data1');
- 	allRows.each((index, element) => {
-  	const tds = $(element).find('td');
-  	const rowURL = "https://www.espncricinfo.com" +$(tds[6]).find('a').attr('href');
-  	links.push(rowURL);
- })
-return {
-  'playersLinks': links
-};
+    def parse(self, response):
+        # Parsing the initial page to extract players' links
+        links = response.css('table.engineTable > tbody > tr.data1')
+        for row in links:
+            row_url = "https://www.espncricinfo.com" + row.css('td:nth-child(7) a::attr(href)').get()
+            yield scrapy.Request(url=row_url, callback=self.parse_players_links)
 
-/* -------------- STAGE 2 ------------ */
+    def parse_players_links(self, response):
+        # Parsing individual player's page to collect bowling summary links
+        links = []
+        all_rows = response.css('table.engineTable > tbody > tr.data1')
+        for row in all_rows:
+            tds = row.css('td')
+            row_url = "https://www.espncricinfo.com" + tds[6].css('a::attr(href)').get()
+            links.append(row_url)
 
-//------- 2.a Interaction Code ------ //
-navigate(input.url);
-collect(parse());
+        yield {"playersLinks": links}
 
-//---------- 2.b Parser Code ---------//
-var match = $('div').filter(function(){
-	return $(this)
-      .find('span > span > span').text() === String("Match Details") 
-}).siblings()
-team1 = $(match.eq(0)).find('span > span > span').text().replace(" Innings", "")
-team2 = $(match.eq(1)).find('span > span > span').text().replace(" Innings", "")
-matchInfo = team1 + ' Vs ' + team2
+    def parse(self, response):
+        # Parsing the player's page to collect bowling summary
+        match = response.css('div:has(span > span > span:contains("Match Details")) + div')
+        team1 = match.css('span > span > span::text').getall()[0].replace(" Innings", "")
+        team2 = match.css('span > span > span::text').getall()[1].replace(" Innings", "")
+        match_info = team1 + ' Vs ' + team2
 
-var tables = $('div > table.ds-table');
-var firstInningRows = $(tables.eq(1)).find('tbody > tr').filter(function(index, element){
-  return $(this).find("td").length >= 11
-})
+        tables = response.css('div > table.ds-table')
+        first_inning_rows = tables[1].css('tbody > tr').xpath('count(td) >= 11')
 
-var secondInningsRows = $(tables.eq(3)).find('tbody > tr').filter(function(index, element){
-  return $(this).find("td").length >= 11
-});
+        second_inning_rows = tables[3].css('tbody > tr').xpath('count(td) >= 11')
 
+        bowling_summary = []
+        # Parsing bowling summary for the first innings
+        for index, row in enumerate(first_inning_rows):
+            tds = row.css('td')
+            bowling_summary.append({
+                "match": match_info,
+                "bowlingTeam": team2,
+                "bowlerName": tds[0].css('a > span::text').get().replace(' ', ''),
+                "overs": tds[1].css('::text').get(),
+                "maiden": tds[2].css('::text').get(),
+                "runs": tds[3].css('::text').get(),
+                "wickets": tds[4].css('::text').get(),
+                "economy": tds[5].css('::text').get(),
+                "0s": tds[6].css('::text').get(),
+                "4s": tds[7].css('::text').get(),
+                "6s": tds[8].css('::text').get(),
+                "wides": tds[9].css('::text').get(),
+                "noBalls": tds[10].css('::text').get()
+            })
 
-var bowlingSummary = []
-firstInningRows.each((index, element) => {
-  var tds = $(element).find('td');
-  bowlingSummary.push({
-  		"match": matchInfo,
-  		"bowlingTeam": team2,
-   		"bowlerName": $(tds.eq(0)).find('a > span').text().replace(' ', ''),
-    	"overs": $(tds.eq(1)).text(),
-  		"maiden": $(tds.eq(2)).text(), 
-  		"runs": $(tds.eq(3)).text(),
-  		"wickets": $(tds.eq(4)).text(),
-  		"economy": $(tds.eq(5)).text(),
- 		"0s": $(tds.eq(6)).text(),
-    	"4s": $(tds.eq(7)).text(),
-    	"6s": $(tds.eq(8)).text(),
-    	"wides": $(tds.eq(9)).text(),
-    	"noBalls": $(tds.eq(10)).text()
-  });
-});
+        # Parsing bowling summary for the second innings
+        for index, row in enumerate(second_inning_rows):
+            tds = row.css('td')
+            bowling_summary.append({
+                "match": match_info,
+                "bowlingTeam": team1,
+                "bowlerName": tds[0].css('a > span::text').get().replace(' ', ''),
+                "overs": tds[1].css('::text').get(),
+                "maiden": tds[2].css('::text').get(),
+                "runs": tds[3].css('::text').get(),
+                "wickets": tds[4].css('::text').get(),
+                "economy": tds[5].css('::text').get(),
+                "0s": tds[6].css('::text').get(),
+                "4s": tds[7].css('::text').get(),
+                "6s": tds[8].css('::text').get(),
+                "wides": tds[9].css('::text').get(),
+                "noBalls": tds[10].css('::text').get()
+            })
 
-secondInningsRows.each((index, element) => {
-  var tds = $(element).find('td');
-   bowlingSummary.push({
-  		"match": matchInfo,
-  		"bowlingTeam": team1,
-   		"bowlerName": $(tds.eq(0)).find('a > span').text().replace(' ', ''),
-    	"overs": $(tds.eq(1)).text(),
-  		"maiden": $(tds.eq(2)).text(), 
-  		"runs": $(tds.eq(3)).text(),
-  		"wickets": $(tds.eq(4)).text(),
-  		"economy": $(tds.eq(5)).text(),
- 		"0s": $(tds.eq(6)).text(),
-    	"4s": $(tds.eq(7)).text(),
-    	"6s": $(tds.eq(8)).text(),
-    	"wides": $(tds.eq(9)).text(),
-    	"noBalls": $(tds.eq(10)).text()
-  });
-});
-
-return {"bowlingSummary": bowlingSummary}
-
+        yield {"bowlingSummary": bowling_summary}
